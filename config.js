e91af4952ba77a9d703c070e4cab173b4592f129
Rahul Rajeev <rahulrajeev91@gmail.com>
2014-04-19 17:11:00 +0800

    bootstrap integration, checkers

--- config.js
+++ config.js
@@ -3,5 +3,6 @@
 // http://www.cs.toronto.edu/~arnold/cp3101b/assignments/03/ports.txt
 
 var port=8035; 
-var server = "cp3101b-1.comp.nus.edu.sg";
+//var server = "cp3101b-1.comp.nus.edu.sg";
 //var server = "localhost";
+var server = "192.168.107.1";
