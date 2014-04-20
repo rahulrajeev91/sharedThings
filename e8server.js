6570827efc613bf28e0a379a7c9feba53bbd70cc
Rahul Rajeev <rahulrajeev91@gmail.com>
2014-04-20 20:57:34 +0800

    e8 completed

--- e8server.js
+++ e8server.js
@@ -1,15 +1,34 @@
 /* 
 CP3101B Assignment 3 - exercise 8 Server
 Including World ID and allowing users to choose/create world to share
+
+Input Data : {"type":"<Type of request>",wid":"<NameOfWorld>", "state":"<Hash containing all the changed states>"}
+
+	type:
+		"worldIDChange"	: World ID Changed
+		"getWorldState"	: Get world state
+		"updateWorld"	: sending updated world
+
+Output Data : {"type":"<Type of data>",wid":"<NameOfWorld>", "state":"<Hash containing all the changed states>", "worlds":"<array of all the world names>"}
+
+	type:
+		"worldList"		: Sending World name List
+		"worldState"	: sending world state
+		"updateWorld"	: sending world state changes
 */
 
 var config = require('./config_node.js');
 
 var WebSocketServer = require('ws').Server, wss = new WebSocketServer({port: config.port});
 
-var Changes = {};
+var changedWorld = {};
+var worldId = "";
+var dataType = "";
 var stateChanges = {};
-var Universe = [];		//contains the stored global world 
+var Universe = new Array();		// contains the the states of all the worlds
+var Worlds = new Array();		// array containing the name of all the worlds
+
+var returnData = {};
 
 wss.on('close', function() {
     console.log('disconnected');
@@ -21,33 +40,87 @@ wss.broadcast = function(data,sender) {
         	this.clients[i].send(data);
 };
 
-wss.broadcastAll = function(data,sender) {
+wss.broadcastAll = function(data) {
     for(var i in this.clients)
     	this.clients[i].send(data);
 };
 
 wss.on('connection', function(ws) {
 
-	//wss.broadcastAll(Universe, ws);
-	ws.on('message', function(RecievedWorld) {
-		//World=JSON.parse(RecievedWorld);
-		Changes = JSON.parse(RecievedWorld);
-		console.log('\n============================\nreceived: %s', JSON.stringify(Changes));
+	returnData = {};
+	returnData["type"] = "worldList";
+	returnData["worlds"] = Worlds;
 
-		//update the World
-		stateChanges = Changes["state"];
-		for (var i in Universe)
-		{
-			if(Universe[i].wid == Changes["wid"])
+	ws.send(JSON.stringify(returnData));
+	console.log("new client");
+
+	ws.on('message', function(RecievedWorld) {
+		
+
+		// Display the data //////////////////////////////////////////////////////////////////////////
+		changedWorld = JSON.parse(RecievedWorld);
+		console.log('\n============================\nreceived: %s',JSON.stringify(changedWorld));
+
+		// Extract information from the input data////////////////////////////////////////////////////
+		dataType = changedWorld["type"];
+		stateChanges = changedWorld["state"];
+		worldId = changedWorld["wid"];
+
+		// process data //////////////////////////////////////////////////////////////////////////////
+
+		if (worldId =="")
+			return;
+
+		returnData = {};
+
+		if(dataType=="worldIDChange"||dataType=="getWorldState"){
+			flag=0;
+			for (var i in Universe)
 			{
-				for(var value in stateChanges)
-					Universe[i].state[value] = stateChanges[value];
+				if(Universe[i].wid == worldId)
+				{
+					returnData = Universe[i];
+					returnData["type"] = "worldState";
+					returnData["worlds"] = Worlds;
+					ws.send(JSON.stringify(returnData));	
+					return;	
+				}
 			}
 		}
-		// if(Universe == null)
-		// 	Universe[0] = Changes;
-		//console.log(JSON.stringify(Universe[0].state));	
-		//update only the changees to the world
-		wss.broadcast(JSON.stringify(Changes),ws);	
+		else if(dataType=="updateWorld") {
+			
+			flag=0;
+			for (var i in Universe) {
+				if(Universe[i].wid == worldId) {
+					//searching universe for the correct world ID
+					for(var value in stateChanges)
+						Universe[i].state[value] = stateChanges[value];
+					
+					returnData["type"] = "updateWorld";
+					returnData["state"] = stateChanges;
+					returnData["worlds"] = Worlds;
+					returnData["wid"] = worldId;
+
+					wss.broadcast(JSON.stringify(returnData),ws);	
+					return;
+				}
+			}
+		}
+
+		// checking to make sure if new world has been added
+		console.log("\nAdded to universe : "+ worldId);
+
+		var tempWorld = {};
+		tempWorld["wid"] = worldId;
+		tempWorld["state"] = stateChanges;
+
+		Universe.push(tempWorld);
+		Worlds.push(worldId);
+
+		returnData["type"] = "worldList";
+		returnData["worlds"] = Worlds;
+
+		wss.broadcastAll(JSON.stringify(returnData));
+
 	});
 });
